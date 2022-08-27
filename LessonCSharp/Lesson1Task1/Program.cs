37ab32b3a995466915be7845ea8dd8115322af1e
FlashMax <flashmax537@yandex.ru>
2022-08-27 20:04:39 +0400

    Выполнила задание по уроку номер 1

---
+++ LessonCSharp/Lesson1Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lesson1Task1
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Как Вас зовут?");
+            System.Console.Write("Введите Ваше имя, пожалуйста: ");
+            string name = Console.ReadLine();
+            string date = DateTime.Now.ToShortDateString();
+            System.Console.WriteLine($"Привет, {name}, сегодня {date} года!");
+        }
+    }
+}
